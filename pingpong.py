3903f112106d9924e9a0ed8de9b4dfab24f28bf7
QWERTFN <169074677+QWERTFN@users.noreply.github.com>
2024-05-13 19:02:03 +0300

    создал окно.

--- pingpong.py
+++ pingpong.py
@@ -1,1 +1,17 @@
-from pygamy import *
+from pygame import *
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("pingpong")
+
+game = True
+finish = False
+
+while game:
+   for e in event.get():
+       if e.type == QUIT:
+           game = False
+
+display.update()
+time.delay(50)
+FPS = 60
